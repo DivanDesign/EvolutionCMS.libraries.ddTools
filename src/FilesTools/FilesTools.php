bce6546342242a6a808134573862bfd643c2f202
Ilyas Ronef <ilyas@DivanDesign.ru>
2020-06-17 14:21:38 +0300

    * `\DDTools\FilesTools::modifyImage` → `$params->quality`: Is not required anymore and by default is equal to `100`.

--- src/FilesTools/FilesTools.php
+++ src/FilesTools/FilesTools.php
@@ -148,7 +148,7 @@ class FilesTools {
 	
 	/**
 	 * modifyImage
-	 * @version 2.4a (2020-06-17)
+	 * @version 2.5a (2020-06-17)
 	 * 
 	 * @desc Делает превьюшку.
 	 * 
@@ -160,7 +160,7 @@ class FilesTools {
 	 * @param $params->height {integer} — Высота результирующего изображения. Если задать один размер — второй будет вычислен автоматически исходя из пропорций оригинального изображения. @required
 	 * @param $params->allowEnlargement {0|1} — Разрешить ли увеличение изображения? Default: 0.
 	 * @param $params->backgroundColor {string} — Фон результирующего изображения (может понадобиться для заливки пустых мест при `$params->transformMode` == `'resizeAndFill'`). Default: 'FFFFFF'.
-	 * @param $params->quality {integer} — Output image quality level. @required
+	 * @param $params->quality {integer} — Output image quality level. Default: 100.
 	 * 
 	 * @return {void}
 	 */
@@ -171,7 +171,8 @@ class FilesTools {
 				(object) [
 					'transformMode' => 'resize',
 					'allowEnlargement' => 0,
-					'backgroundColor' => 'FFFFFF'
+					'backgroundColor' => 'FFFFFF',
+					'quality' => 100
 				],
 				$params
 			]
